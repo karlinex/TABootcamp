a359bc4372dc39c2933cb5797fbd33589e5cd7e2
karlinex <karlinex98@gmail.com>
2022-07-25 16:26:42 +0300

    Activity 2

---
+++ TABootcampJavaProject/src/com/bootcampexcersise/module2/activity/OperatorActivity.java
@@ -0,0 +1,9 @@
+package com.bootcampexcersise.module2.activity;
+
+public class OperatorActivity {
+    public static void main(String[] args) {
+        int a = 5, b = 30;
+        int sum = a + b;
+        System.out.println(sum);
+    }
+}
