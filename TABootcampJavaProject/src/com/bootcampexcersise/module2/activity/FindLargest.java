68b5bef9e5e2327a5f50bfec958e48f044085476
karlinex <karlinex98@gmail.com>
2022-07-25 17:06:34 +0300

    Activity 3

---
+++ TABootcampJavaProject/src/com/bootcampexcersise/module2/activity/FindLargest.java
@@ -0,0 +1,13 @@
+package com.bootcampexcersise.module2.activity;
+
+public class FindLargest {
+    public static void main(String[] args) {
+        int a = 19;
+        int b = 22;
+        if (a > b) {
+            System.out.println("a is greater than b");
+        } else {
+            System.out.println("b is greater than a");
+        }
+    }
+}
