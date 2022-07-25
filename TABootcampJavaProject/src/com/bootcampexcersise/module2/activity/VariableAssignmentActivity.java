b0fa91f5f10f0e558710841fbc22dd829d324dc6
karlinex <karlinex98@gmail.com>
2022-07-25 14:39:02 +0300

    Activity1

---
+++ TABootcampJavaProject/src/com/bootcampexcersise/module2/activity/VariableAssignmentActivity.java
@@ -0,0 +1,10 @@
+package com.bootcampexcersise.module2.activity;
+
+public class VariableAssignmentActivity {
+    public static void main(String[] args) {
+        int i;
+        i = 10;
+        i = 20;
+        System.out.print(i);
+    }
+}
