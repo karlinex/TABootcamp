014c861587a8fbd41828de7572f6762a7c38164e
karlinex <karlinex98@gmail.com>
2022-07-26 10:12:41 +0300

    Activity 7

--- TABootcampJavaProject/src/com/bootcampexcersise/module2/activity/MultiplicationTable.java
+++ TABootcampJavaProject/src/com/bootcampexcersise/module2/activity/MultiplicationTable.java
@@ -17,10 +17,8 @@ public class MultiplicationTable {
         }
         // Table from 11 to 20
         for (int i = 11; i <= 20; i++) {
-            //TODO: complete code. write for loop for int i
             System.out.println("*** Table of " + i + " ***");
             for (int j = 11; j <= 20; j++) {
-                //TODO: complete code. write nested for loop for int j
                 System.out.println(i + "x" + j + " = " + i * j);
             }
         }
---
+++ TABootcampJavaProject/src/com/bootcampexcersise/module2/activity/PrintNumWithWhile.java
@@ -0,0 +1,23 @@
+package com.bootcampexcersise.module2.activity;
+
+public class PrintNumWithWhile {
+    public static void main(String[] args) {
+
+        System.out.println("Even numbers:");
+        // Print all even numbers less than 100
+        int i = 0;
+        while (i < 100) {
+            System.out.println(i);
+            i = i + 2;
+        }
+        System.out.println("Odd numbers:");
+        //TODO: write code to Print all odd numbers less than 100
+        i = 0;
+        while (i < 100) {
+            if (i % 2 == 1) {
+            System.out.println(i);
+            }
+            i++;
+        }
+    }
+}
