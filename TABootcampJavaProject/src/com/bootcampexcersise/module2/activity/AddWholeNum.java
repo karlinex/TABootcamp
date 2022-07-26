c5d560afe5ae6007a744f6bb0b518123f9eb9d29
karlinex <karlinex98@gmail.com>
2022-07-26 12:02:59 +0300

    Activity 6 corrections

--- TABootcampJavaProject/src/com/bootcampexcersise/module2/activity/AddWholeNum.java
+++ TABootcampJavaProject/src/com/bootcampexcersise/module2/activity/AddWholeNum.java
@@ -5,7 +5,7 @@ public class AddWholeNum {
     public static void main(String args[]) {
         int sum = 0;
         for (int i = 50; i <= 100; i++) {
-            sum = sum +i;
+            sum = sum + i;
         }
     System.out.println("Sum of numbers between 50 and 100 is " + sum);
     }
--- TABootcampJavaProject/src/com/bootcampexcersise/module2/activity/MultiplicationTable.java
+++ TABootcampJavaProject/src/com/bootcampexcersise/module2/activity/MultiplicationTable.java
@@ -12,13 +12,13 @@ public class MultiplicationTable {
         for (int i = 1; i <= 10; i++) {
             System.out.println("*** Table of "+i + " ***");
             for (int j = 1; j <= 10; j++) {
-                System.out.println(i + "x" + j +" = " + i*j);
+                System.out.println(i + "x" + j +" = " + i * j);
             }
         }
         // Table from 11 to 20
         for (int i = 11; i <= 20; i++) {
             System.out.println("*** Table of " + i + " ***");
-            for (int j = 11; j <= 20; j++) {
+            for (int j = 1; j <= 10; j++) {
                 System.out.println(i + "x" + j + " = " + i * j);
             }
         }
