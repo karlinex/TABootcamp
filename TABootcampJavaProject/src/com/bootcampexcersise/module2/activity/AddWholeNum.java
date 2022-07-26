5089c48a050d575347ccddaad4deafeebf0a32b6
karlinex <karlinex98@gmail.com>
2022-07-26 09:42:51 +0300

    Activity 6

--- TABootcampJavaProject/src/com/bootcampexcersise/module2/activity/AddWholeNum.java
+++ TABootcampJavaProject/src/com/bootcampexcersise/module2/activity/AddWholeNum.java
@@ -2,7 +2,7 @@ package com.bootcampexcersise.module2.activity;
 
 public class AddWholeNum {
 
-    public static void main(String[] args) {
+    public static void main(String args[]) {
         int sum = 0;
         for (int i = 50; i <= 100; i++) {
             sum = sum +i;
---
+++ TABootcampJavaProject/src/com/bootcampexcersise/module2/activity/MultiplicationTable.java
@@ -0,0 +1,28 @@
+package com.bootcampexcersise.module2.activity;
+
+public class MultiplicationTable {
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+
+
+
+        // Table from 1 to 10
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("*** Table of "+i + " ***");
+            for (int j = 1; j <= 10; j++) {
+                System.out.println(i + "x" + j +" = " + i*j);
+            }
+        }
+        // Table from 11 to 20
+        for (int i = 11; i <= 20; i++) {
+            //TODO: complete code. write for loop for int i
+            System.out.println("*** Table of " + i + " ***");
+            for (int j = 11; j <= 20; j++) {
+                //TODO: complete code. write nested for loop for int j
+                System.out.println(i + "x" + j + " = " + i * j);
+            }
+        }
+    }
+}
