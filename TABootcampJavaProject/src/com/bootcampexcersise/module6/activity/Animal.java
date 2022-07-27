676d79f82c23723f4cd23694fa57f3612889df44
karlinex <karlinex98@gmail.com>
2022-07-27 16:57:42 +0300

    Interfaces task

---
+++ TABootcampJavaProject/src/com/bootcampexcersise/module6/activity/Animal.java
@@ -0,0 +1,7 @@
+package com.bootcampexcersise.module6.activity;
+
+public interface Animal {
+
+    public void animalSound();
+    public void sleep();
+}
---
+++ TABootcampJavaProject/src/com/bootcampexcersise/module6/activity/AnimalActivity.java
@@ -0,0 +1,29 @@
+package com.bootcampexcersise.module6.activity;
+
+public class AnimalActivity {
+
+    public static void main(String arg[]) {
+
+        //TODO: Use interfaces to declare identifiers instead of the actual classes
+        Animal cat = new Cat();
+        Animal dog = new Dog();
+        //TODO: Create Dog using interface Animal
+
+
+
+        Mammal mammal = new Mammal("Flexy");
+
+        //TODO: For mammal set animal to cat
+        mammal.setAnimal(cat);
+        //TODO: execute for mammal animalIsSleeping()
+        mammal.animalIsSleeping();
+        //TODO: execute for mammal animalIsSpeaking()
+        mammal.animalIsSpeaking();
+        //TODO: For mammal set animal to dog
+        mammal.setAnimal(dog);
+        //TODO: execute for mammal animalIsSleeping()
+        mammal.animalIsSleeping();
+        //TODO: execute for mammal animalIsSpeaking()
+        mammal.animalIsSpeaking();
+    }
+}
