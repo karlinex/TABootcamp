1409ef519de7c30d90b0815983298e57a0a8d656
karlinex <karlinex98@gmail.com>
2022-07-29 11:31:52 +0300

    MapActivity

---
+++ TABootcampJavaProject/src/com/bootcampexcersise/module8/MapActivity.java
@@ -0,0 +1,29 @@
+package com.bootcampexcersise.module8;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+
+public class MapActivity {
+
+    public static void main(String[] args) {
+        //1 - Type code to create a HashMap of key value pair
+        //where key is id of type String and value is a name
+        Map map = new HashMap<String, Integer>();
+        map.put("key1", 123);
+        map.put("key2", 456);
+        map.put("key3", 789);
+
+        //2 - Call print method to print the map passed as its parameter.
+        print(map);
+    }
+
+    static void print(Map<String, Integer> map) {
+        for(Map.Entry<String, Integer> entry : map.entrySet()) {
+            entry.getKey();
+            System.out.println(entry.getKey() + " = " + entry.getValue());
+        }
+        //3 - Type code to print this map
+    }
+}
